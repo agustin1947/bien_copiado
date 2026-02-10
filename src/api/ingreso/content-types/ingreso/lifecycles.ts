4a7e851a0b59fc33e566e895deb551401d680b7f
jesustissera <jesustissera@gmail.com>
2026-02-10 17:09:33 -0300

    feat (BE): Add validation to prevent order numbers from being edited

--- src/api/ingreso/content-types/ingreso/lifecycles.ts
+++ src/api/ingreso/content-types/ingreso/lifecycles.ts
@@ -1,5 +1,6 @@
 import { errors } from "@strapi/utils";
 import { factories } from "@strapi/strapi";
+import ingreso from "../../controllers/ingreso";
 
 export default {
   async beforeCreate(event) {
@@ -66,15 +67,7 @@ export default {
         );
       }
 
-      // validar forma de pago
-      if (ordenST.forma_de_pago?.id !== ctxBody.forma_de_pago.connect[0].id) {
-        throw new errors.ApplicationError(
-          `El número de orden de Servicio Técnico ingresado tiene una forma de pago diferente a la seleccionada en el ingreso`,
-        );
-      }
-
       // validar montos
-
       // obtener todos los ingresos que tengan ese número de orden de servicio técnico para sumar sus montos
       const ingresosRelacionados = await strapi.db
         .query("api::ingreso.ingreso")
@@ -85,7 +78,7 @@ export default {
       const pagosParciales = ingresosRelacionados.reduce((total, ingreso) => {
         return total + parseFloat(ingreso.monto);
       }, 0);
-
+      
       //el monto obtenido de los ingresos restarlo al monto total del servicio técnico
       const diferencia = parseFloat(ordenST.total) - pagosParciales;
 
@@ -106,7 +99,62 @@ export default {
   },
   async afterCreate(event) {},
   async beforeUpdate(event) {
+    const ctx = strapi.requestContext.get();
+    const ctxBody = ctx.request.body;
+    const { params } = event;
+    const ingresoId = params.where.id;
+
+    console.log("ctxBody", ctxBody);
+    console.log("params", params);
     //(strapi as any).io.emit("refresh", "actualizado");
+
+    const ingresoOriginal = await strapi.db
+        .query("api::ingreso.ingreso")
+        .findOne({
+          where: { id: ingresoId },
+          populate: true,
+        });
+
+    console.log("ingresoOriginal", ingresoOriginal);    
+    // que no se carguen números de orden de cuenta corriente y servicio técnico al mismo tiempo
+    if (ctxBody.n_orden_st && ctxBody.n_orden_cc) {
+      throw new errors.ApplicationError(
+        `No puede ingresar número de orden de Cuenta Corriente y Servicio Técnico al mismo tiempo, debe elegir una o ninguna de las dos opciones`,
+      );
+    }
+    
+    // que no se modifique el n de orden
+    /** si se cargo en principio n de orden de st */
+    if(ingresoOriginal.n_orden_st){
+        if(ctxBody.n_orden_st !== ingresoOriginal.n_orden_st){
+            throw new errors.ApplicationError(
+                `No se puede modificar el número de orden de Servicio Técnico una vez creado el ingreso`,
+              );
+        }
+    }
+
+    /** si se cargo en principio n de orden de cc */
+    if(ingresoOriginal.n_orden_cc){ 
+        if(ctxBody.n_orden_cc !== ingresoOriginal.n_orden_cc){ 
+            throw new errors.ApplicationError(
+                `No se puede modificar el número de orden de Cuenta Corriente una vez creado el ingreso`,
+              );
+        }
+    }
+    
+
+    // que se seleccione un local y que no se modfique
+    // que se seleccione un tipo de moneda y que no se modifique
+    // que se seleccione una forma de pago
+
+    //validar montos
+    // obtener todos los ingresos que tengan ese número de orden de servicio técnico para sumar sus montos
+    //el monto obtenido de los ingresos restarlo al monto total del servicio técnico
+    // en caso que la diferencia cea cero, quiere decir que ya se termino de pagar.
+    // el monto del ingreso tiene que ser menor o igual a esta diferencia
+
+    throw new errors.ApplicationError(`BEFORE UPDATE`,);
+
   },
   async afterUpdate(event) {},
 };
