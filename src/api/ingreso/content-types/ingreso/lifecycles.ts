e2f627749de00b51f7044d8658dda3753e4f0694
jesustissera <jesustissera@gmail.com>
2026-02-11 11:08:20 -0300

    feat (BE): Add validation between input and order

--- src/api/ingreso/content-types/ingreso/lifecycles.ts
+++ src/api/ingreso/content-types/ingreso/lifecycles.ts
@@ -1,6 +1,7 @@
 import { errors } from "@strapi/utils";
 import { factories } from "@strapi/strapi";
 import ingreso from "../../controllers/ingreso";
+import { get } from "http";
 
 export default {
   async beforeCreate(event) {
@@ -42,58 +43,9 @@ export default {
 
     // en caso que se complete número de orden de un servicio técnico.
     if (ctxBody.n_orden_st && !ctxBody.n_orden_cc) {
-      const ordenST = await strapi.db.query("api::service.service").findOne({
-        where: { id: ctxBody.n_orden_st },
-        populate: true,
-      });
-
-      if (!ordenST) {
-        throw new errors.ApplicationError(
-          `El número de orden de Servicio Técnico ingresado no existe`,
-        );
-      }
-
-      // los service siempre son en pesos
-      if (ctxBody.tipo_de_moneda.connect[0].id !== 1) {
-        throw new errors.ApplicationError(
-          `El número de orden de Servicio Técnico ingresado tiene un tipo de moneda diferente al seleccionado en el ingreso`,
-        );
-      }
-
-      // validar si tienen el mismo local
-      if (ordenST.local?.id !== ctxBody.local.connect[0].id) {
-        throw new errors.ApplicationError(
-          `El número de orden de Servicio Técnico ingresado tiene un local diferente al seleccionado en el ingreso`,
-        );
-      }
-
-      // validar montos
-      // obtener todos los ingresos que tengan ese número de orden de servicio técnico para sumar sus montos
-      const ingresosRelacionados = await strapi.db
-        .query("api::ingreso.ingreso")
-        .findMany({
-          where: { n_orden_st: ctxBody.n_orden_st },
-        });
-
-      const pagosParciales = ingresosRelacionados.reduce((total, ingreso) => {
-        return total + parseFloat(ingreso.monto);
-      }, 0);
-      
-      //el monto obtenido de los ingresos restarlo al monto total del servicio técnico
-      const diferencia = parseFloat(ordenST.total) - pagosParciales;
-
-      // en caso que la diferencia cea cero, quiere decir que ya se termino de pagar.
-      if (diferencia === 0) {
-        throw new errors.ApplicationError(
-          `El pago del Servicio Técnico ya está completado`,
-        );
-      }
-
-      // el monto del ingreso tiene que ser menor o igual a esta diferencia
-      if (ctxBody.monto > diferencia) {
-        throw new errors.ApplicationError(
-          `El monto del ingreso excede la diferencia restante del Servicio Técnico`,
-        );
+      const diff = await validateEntryAgainstOrder(ctxBody, "api::service.service");
+      if (diff.error) {
+        throw new errors.ApplicationError(diff.message);
       }
     }
   },
@@ -104,20 +56,15 @@ export default {
     const { params } = event;
     const ingresoId = params.where.id;
 
-    console.log("ctxBody", ctxBody);
-    //(strapi as any).io.emit("refresh", "actualizado");
-
     const ingresoOriginal = await strapi.db
-        .query("api::ingreso.ingreso")
-        .findOne({
-          where: { id: ingresoId },
-          populate: true,
-        });
+      .query("api::ingreso.ingreso")
+      .findOne({
+        where: { id: ingresoId },
+        populate: true,
+      });
 
-    console.log("ingresoOriginal", ingresoOriginal);
     const localOriginal = ingresoOriginal.local?.id;
     const tipoDeMonedaOriginal = ingresoOriginal.tipo_de_moneda?.id;
-    const formaDePagoOriginal = ingresoOriginal.forma_de_pago?.id;
 
     // que no se carguen números de orden de cuenta corriente y servicio técnico al mismo tiempo
     if (ctxBody.n_orden_st && ctxBody.n_orden_cc) {
@@ -125,50 +72,142 @@ export default {
         `No puede ingresar número de orden de Cuenta Corriente y Servicio Técnico al mismo tiempo, debe elegir una o ninguna de las dos opciones`,
       );
     }
-    
-    // que no se modifique el n de orden
+
+    /** EVITAR EL CAMBIO DE N DE ORDEN */
     /** si se cargo en principio n de orden de st */
-    if(ingresoOriginal.n_orden_st){
-        if(ctxBody.n_orden_st !== ingresoOriginal.n_orden_st){
-            throw new errors.ApplicationError(
-                `No se puede modificar el número de orden de Servicio Técnico una vez creado el ingreso`,
-              );
-        }
+    if (ingresoOriginal.n_orden_st) {
+      if (ctxBody.n_orden_st !== ingresoOriginal.n_orden_st) {
+        throw new errors.ApplicationError(
+          `No se puede modificar el número de orden de Servicio Técnico una vez creado el ingreso`,
+        );
+      }
     }
 
     /** si se cargo en principio n de orden de cc */
-    if(ingresoOriginal.n_orden_cc){ 
-        if(ctxBody.n_orden_cc !== ingresoOriginal.n_orden_cc){ 
-            throw new errors.ApplicationError(
-                `No se puede modificar el número de orden de Cuenta Corriente una vez creado el ingreso`,
-              );
-        }
+    if (ingresoOriginal.n_orden_cc) {
+      if (ctxBody.n_orden_cc !== ingresoOriginal.n_orden_cc) {
+        throw new errors.ApplicationError(
+          `No se puede modificar el número de orden de Cuenta Corriente una vez creado el ingreso`,
+        );
+      }
     }
-    
 
     // que se seleccione un local y que no se modfique
-    if((ctxBody.local.connect.length > 0 && ctxBody.local.connect[0].id !== localOriginal) || 
-        (ctxBody.local.connect.length === 0 && ctxBody.local.disconnect.length > 0)){
-        throw new errors.ApplicationError(`No se puede modificar el "Local" una vez creado el ingreso`);
+    if (
+      (ctxBody.local.connect.length > 0 &&
+        ctxBody.local.connect[0].id !== localOriginal) ||
+      (ctxBody.local.connect.length === 0 &&
+        ctxBody.local.disconnect.length > 0)
+    ) {
+      throw new errors.ApplicationError(
+        `No se puede modificar el "Local" una vez creado el ingreso`,
+      );
     }
 
     // que se seleccione un tipo de moneda y que no se modifique
-    if((ctxBody.tipo_de_moneda.connect.length > 0 && ctxBody.tipo_de_moneda.connect[0].id !== tipoDeMonedaOriginal) || 
-        (ctxBody.tipo_de_moneda.connect.length === 0 && ctxBody.tipo_de_moneda.disconnect.length > 0)){
-        throw new errors.ApplicationError(`No se puede modificar el "Tipo de Moneda" una vez creado el ingreso`);
+    if (
+      (ctxBody.tipo_de_moneda.connect.length > 0 &&
+        ctxBody.tipo_de_moneda.connect[0].id !== tipoDeMonedaOriginal) ||
+      (ctxBody.tipo_de_moneda.connect.length === 0 &&
+        ctxBody.tipo_de_moneda.disconnect.length > 0)
+    ) {
+      throw new errors.ApplicationError(
+        `No se puede modificar el "Tipo de Moneda" una vez creado el ingreso`,
+      );
     }
     // que se seleccione una forma de pago
-    if(ctxBody.forma_de_pago.connect.length === 0 && ctxBody.forma_de_pago.disconnect.length > 0){
-        throw new errors.ApplicationError(`Debe ingresar al menos una "Forma de pago"`);
+    if (
+      ctxBody.forma_de_pago.connect.length === 0 &&
+      ctxBody.forma_de_pago.disconnect.length > 0
+    ) {
+      throw new errors.ApplicationError(
+        `Debe ingresar al menos una "Forma de pago"`,
+      );
     }
-    //validar montos
-    // obtener todos los ingresos que tengan ese número de orden de servicio técnico para sumar sus montos
-    //el monto obtenido de los ingresos restarlo al monto total del servicio técnico
-    // en caso que la diferencia cea cero, quiere decir que ya se termino de pagar.
-    // el monto del ingreso tiene que ser menor o igual a esta diferencia
 
-    throw new errors.ApplicationError(`BEFORE UPDATE`,);
-
+    if (ctxBody.n_orden_st && !ctxBody.n_orden_cc) {
+      const diff = await validateEntryAgainstOrder(ctxBody, "api::service.service", ingresoId);
+      if (diff.error) {
+        throw new errors.ApplicationError(diff.message);
+      }
+    }
   },
   async afterUpdate(event) {},
+};
+
+const validateEntryAgainstOrder = async (ctxBody, api, ingresoId = null) => {
+  const name =
+    api === "api::service.service" ? "Servicio Técnico" : "Cuenta Corriente";
+  const n_orden =
+    api === "api::service.service" ? ctxBody.n_orden_st : ctxBody.n_orden_cc;
+
+  const orden = await strapi.db.query(api).findOne({
+    where: { id: n_orden },
+    populate: true,
+  });
+
+  if (!orden) {
+    return {
+      error: true,
+      message: `${name}: El número de orden ingresado no existe.`,
+    };
+  }
+
+  // los service siempre son en pesos
+  if (
+    api === "api::service.service" &&
+    ctxBody.tipo_de_moneda.connect[0] &&
+    ctxBody.tipo_de_moneda.connect[0].id !== 1
+  ) {
+    return {
+      error: true,
+      message: `${name}: El número de orden ingresado tiene un tipo de moneda diferente al seleccionado en el ingreso.`,
+    };  
+  }
+
+  // validar si tienen el mismo local
+  if (ctxBody.local.connect[0] && orden.local?.id !== ctxBody.local.connect[0].id) {
+    return {
+      error: true,
+      message: `${name}: El número de orden ingresado tiene un local diferente al seleccionado en el ingreso.`,
+    };
+  }
+
+  const n_orden_ingresos_relacionados =
+    api === "api::service.service"
+      ? { n_orden_st: ctxBody.n_orden_st }
+      : { n_orden_cc: ctxBody.n_orden_cc };
+
+  const whereClause = {
+    ...n_orden_ingresos_relacionados,
+    ...(ingresoId && { id: { $ne: ingresoId } }),
+  };
+
+  const ingresosRelacionados = await strapi.db
+    .query("api::ingreso.ingreso")
+    .findMany({
+      where: whereClause,
+    });
+
+  const pagosParciales = ingresosRelacionados.reduce((total, ingreso) => {
+    return total + parseFloat(ingreso.monto);
+  }, 0);
+
+  const diferencia = parseFloat(orden.total) - pagosParciales;
+
+  if (diferencia === 0) {
+    return {
+      error: true,
+      message: `${name}: el pago ya está completado`,
+    };
+  }
+
+  if (ctxBody.monto > diferencia) {
+    return {
+      error: true,
+      message: `${name}: el monto del ingreso excede la diferencia restante`,
+    };
+  }
+
+  return { error: false };
 };
