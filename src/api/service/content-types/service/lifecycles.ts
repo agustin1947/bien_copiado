db4008377e5755d4c5891e697acb9262e4cfa906
jesustissera <jesustissera@gmail.com>
2026-02-12 16:38:46 -0300

    feat: remove service-related revenue

--- src/api/service/content-types/service/lifecycles.ts
+++ src/api/service/content-types/service/lifecycles.ts
@@ -3,7 +3,7 @@ import { errors } from "@strapi/utils";
 export default {
   async beforeCreate(event) {
     const { data } = event.params;
-    
+
     if (
       !data.local ||
       data.local.length === 0 ||
@@ -18,7 +18,7 @@ export default {
       data.estado_de_service.connect.length === 0
     ) {
       throw new errors.ApplicationError(
-        `Debe seleccionar un estado para el servicio técnico.`
+        `Debe seleccionar un estado para el servicio técnico.`,
       );
     }
 
@@ -33,30 +33,29 @@ export default {
   async afterCreate(event) {
     const { result } = event;
     const { data } = event.params;
-    
+
     if (!result.numero_de_orden) {
       await strapi.db.query("api::service.service").update({
         where: { id: result.id },
-        data: { 
+        data: {
           numero_de_orden: result.id,
-          local:{ connect: [], disconnect: [] },
-          forma_de_pago:{ connect: [], disconnect: [] },
-          estado_de_service:{ connect: [], disconnect: [] }
+          local: { connect: [], disconnect: [] },
+          forma_de_pago: { connect: [], disconnect: [] },
+          estado_de_service: { connect: [], disconnect: [] },
         },
       });
-
     }
     (strapi as any).io.emit("respuesta", "actualizado");
     (strapi as any).io.emit("refresh", "actualizado");
   },
   async beforeUpdate(event) {
     const { data, where } = event.params;
-    
+
     if (data.local.connect.length === 0 && data.local.disconnect.length > 0) {
       throw new errors.ApplicationError(`Debe seleccionar un local.`);
     }
     if (data.local.connect.length > 0 && data.local.disconnect.length > 0) {
-      if (data.local.connect[0].id  !== data.local.disconnect[0].id) {
+      if (data.local.connect[0].id !== data.local.disconnect[0].id) {
         throw new errors.ApplicationError(`No se puede editar el local.`);
       }
     }
@@ -72,9 +71,16 @@ export default {
     ) {
       throw new errors.ApplicationError(`Debe seleccionar una forma de pago.`);
     }
-    if (data.forma_de_pago.connect.length > 0 && data.forma_de_pago.disconnect.length > 0) {
-      if (data.forma_de_pago.connect[0].id  !== data.forma_de_pago.disconnect[0].id) {
-        throw new errors.ApplicationError(`No se puede editar la forma de pago.`);
+    if (
+      data.forma_de_pago.connect.length > 0 &&
+      data.forma_de_pago.disconnect.length > 0
+    ) {
+      if (
+        data.forma_de_pago.connect[0].id !== data.forma_de_pago.disconnect[0].id
+      ) {
+        throw new errors.ApplicationError(
+          `No se puede editar la forma de pago.`,
+        );
       }
     }
 
@@ -85,6 +91,13 @@ export default {
   async afterUpdate(event) {
     (strapi as any).io.emit("respuesta", "actualizado");
     (strapi as any).io.emit("refresh", "actualizado");
+  },
+  async beforeDelete(event) {
+    const { where } = event.params;
+    const serviceId = where.id;
 
+    await strapi.db.query("api::ingreso.ingreso").deleteMany({
+      where: { n_orden_st: serviceId },
+    });
   },
 };
