b1779da58c5d3b17900d9050f5ab92fb5e88b270
jesustissera <jesustissera@gmail.com>
2026-02-18 10:27:06 -0300

    refactor: add mandatory validation for cliente

--- src/api/cuenta-corriente/content-types/cuenta-corriente/lifecycles.ts
+++ src/api/cuenta-corriente/content-types/cuenta-corriente/lifecycles.ts
@@ -3,6 +3,16 @@ const { ApplicationError } = errors;
 
 export default {
   async beforeCreate(event) {
+    const ctx = strapi.requestContext.get();
+    const ctxBody = ctx.request.body;
+    //console.log("ctxBody: ", ctxBody)
+    if (
+      !ctxBody.cliente ||
+      ctxBody.cliente === null ||
+      (ctxBody.cliente.connect && ctxBody.cliente.connect.length === 0)
+    ) {
+      throw new errors.ApplicationError(`Debe seleccionar un "Cliente"`);
+    }
   },
   async afterCreate(event) {},
 };
