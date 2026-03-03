4f885f6e76b3fb62d00075bfe86013891c339821
jesustissera <jesustissera@gmail.com>
2026-03-03 09:54:55 -0300

    style: actualizo posición de los elementos bloqueados del menú.

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -535,11 +535,11 @@ export default {
     bloquearBotones();
     const style = document.createElement("style");
     style.innerHTML = `
-      nav ol li ol li:nth-child(10),
-      nav ol li ol li:nth-child(16),
+      nav ol li ol li:nth-child(11),
+      nav ol li ol li:nth-child(17),
       nav ul li:nth-child(n+3),
       nav:not([aria-label="Pagination"]) ol li span {
-          display: none !important;
+        display: none !important;
       }
      .hidden-delete {
       display: none;
