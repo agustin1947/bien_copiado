149bdef26e6d4ea7bf90f7270fb58547c0f2ef11
jesustissera <jesustissera@gmail.com>
2026-03-26 11:29:24 -0300

    feat: creo funcion para generar reporte

--- src/api/reporte/controllers/reporte.ts
+++ src/api/reporte/controllers/reporte.ts
@@ -43,4 +43,73 @@ export default {
 
     ctx.body = data;
   },
+  async exportCajaMensual(ctx) {
+    try {
+      const { year, month, local } = ctx.query;
+      let name = `reporte_${year}_${month}`;
+      const data = await strapi.service("api::reporte.caja").getCajaMensual({
+        year: Number(year),
+        month: Number(month),
+        localId: local ? Number(local) : null,
+      });
+      
+      if(data.local){
+        name += `_${data.local.nombre}`
+      }
+
+      const csv = buildCsv(data, name);
+      
+      ctx.set("Content-Type", "text/csv");
+      ctx.set(
+        "Content-Disposition",
+        `attachment; filename="${name}.csv"`,
+      );
+
+      ctx.body = csv;
+    } catch (error) {
+      console.error(error);
+      ctx.throw(500, "Error exportando CSV");
+    }
+  },
+};
+
+const buildCsv = (data: any, title: any) => {
+  let csv = "";
+  csv += `${title}\n\n`;
+  // =========================
+  // 1. TOTALES POR MEDIO DE PAGO
+  // =========================
+  csv += "TOTALES POR MEDIO DE PAGO\n";
+  csv += "MEDIO,ENTRADAS ARS,SALIDAS ARS,ENTRADAS USD,SALIDAS USD\n";
+
+  const r = data.resumen;
+
+  csv += `EFECTIVO,${r.entradas.totalEnPesosEfectivo},${r.salidas.totalEnPesosEfectivo},${r.entradas.totalEnDolaresEfectivo},${r.salidas.totalEnDolaresEfectivo}\n`;
+  csv += `TRANSFERENCIA,${r.entradas.totalEnPesosTransferencia},${r.salidas.totalEnPesosTransferencia},${r.entradas.totalEnDolaresTransferencia},${r.salidas.totalEnDolaresTransferencia}\n`;
+  csv += `TARJETA DEBITO,${r.entradas.totalEnPesosTarjetaDeDebito},${r.salidas.totalEnPesosTarjetaDeDebito},${r.entradas.totalEnDolaresTarjetaDeDebito},${r.salidas.totalEnDolaresTarjetaDeDebito}\n`;
+  csv += `TARJETA CREDITO,${r.entradas.totalEnPesosTarjetaDeCredito},${r.salidas.totalEnPesosTarjetaDeCredito},${r.entradas.totalEnDolaresTarjetaDeCredito},${r.salidas.totalEnDolaresTarjetaDeCredito}\n`;
+
+  // =========================
+  // 2. RESUMEN POR DÍA
+  // =========================
+  csv += "\n\nRESUMEN POR DIA\n";
+  csv += "FECHA,ENTRADAS ARS,SALIDAS ARS,ENTRADAS USD,SALIDAS USD\n";
+
+  data.porDia.forEach((d: any) => {
+    const [y, m, day] = d.fecha.split("-");
+    const fechaFormateada = `${day}/${m}/${y}`;
+
+    csv += `${fechaFormateada},${d.ingresosARS},${d.egresosARS},${d.ingresosUSD},${d.egresosUSD}\n`;
+  });
+
+  // =========================
+  // 3. CAJA (SOLO EFECTIVO)
+  // =========================
+  csv += "\n\nRESUMEN CAJA (EFECTIVO)\n";
+  csv += "MONEDA,ENTRADAS,SALIDAS\n";
+
+  csv += `ARS,${r.entradas.totalEnPesosEfectivo},${r.salidas.totalEnPesosEfectivo}\n`;
+  csv += `USD,${r.entradas.totalEnDolaresEfectivo},${r.salidas.totalEnDolaresEfectivo}\n`;
+
+  return csv;
 };
