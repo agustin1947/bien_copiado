a0a36fb39a2317a1ba077793c6457d3b92c2006e
jesustissera <jesustissera@gmail.com>
2026-05-25 12:01:04 -0300

    feat: agregar validación de local en lifecycle

--- src/api/caja-diaria/content-types/caja-diaria/lifecycles.ts
+++ src/api/caja-diaria/content-types/caja-diaria/lifecycles.ts
@@ -1,56 +1,75 @@
 import { errors } from "@strapi/utils";
+import { validateLocalPermissions } from "../../../../utils/validateLocalPermissions";
 
 export default {
   async beforeCreate(event) {
-    if(! event.params.data.fecha_de_ingreso){
+    const ctx = strapi.requestContext.get();
+    const user = ctx.state.user;
+
+    if (!event.params.data.fecha_de_ingreso) {
       throw new errors.ApplicationError(
-        `Debe seleccionar una fecha de ingreso.`
+        `Debe seleccionar una fecha de ingreso.`,
       );
     }
-    
-    const dateOfEntry = new Date(event.params.data.fecha_de_ingreso).toISOString().split("T")[0]; // "2025-12-21";
-    
-    if(!event.params.data.local || event.params.data.local.connect.length === 0) {
-      throw new errors.ApplicationError(
-        `Debe seleccionar un local.`
-      );      
+
+    const dateOfEntry = new Date(event.params.data.fecha_de_ingreso)
+      .toISOString()
+      .split("T")[0]; // "2025-12-21";
+
+    if (
+      !event.params.data.local ||
+      event.params.data.local.connect.length === 0
+    ) {
+      throw new errors.ApplicationError(`Debe seleccionar un local.`);
     }
+
     const localId = event.params.data.local.connect[0].id;
-    
+    validateLocalPermissions(user, localId);
+
     const cajaExistente = await strapi.db
       .query("api::caja-diaria.caja-diaria")
       .findOne({
         where: {
           fecha_de_ingreso: dateOfEntry,
           local: {
-            id: localId,  // <--- acá filtrás por ID
-          }
+            id: localId, // <--- acá filtrás por ID
+          },
         },
         populate: true,
       });
-      
+
     if (cajaExistente) {
       throw new errors.ApplicationError(
-        `Ya existe una caja diaria creada hoy (${cajaExistente.documentId}).`
+        `Ya existe una caja diaria creada hoy (${cajaExistente.documentId}).`,
       );
     }
   },
   async beforeUpdate(event) {
     const { data } = event.params;
+    const ctx = strapi.requestContext.get();
+    const user = ctx.state.user;
 
     const caja = await strapi.db.query("api::caja-diaria.caja-diaria").findOne({
       where: {
         documentId: data.documentId,
       },
-      populate:true
+      populate: true,
     });
-   
-    if(!data.local || data.local.connect.length === 0 && data.local.disconnect.length > 0) {
-      throw new errors.ApplicationError(
-        `Debe seleccionar un local.`
-      );      
+
+    if (
+      !data.local ||
+      (data.local.connect.length === 0 && data.local.disconnect.length > 0)
+    ) {
+      throw new errors.ApplicationError(`Debe seleccionar un local.`);
     }
-    
+
+    if (data.local?.connect && data.local?.connect.length > 0) {
+      console.log("EDITAR CAJA DIARIA");
+      console.log("CTX BODY: ", data)
+      const localId = data.local.connect[0].id;
+      validateLocalPermissions(user, localId);
+    }
+
     /*if(data.local?.connect.length > 0 && data.local?.connect[0].id != caja.local.id) {
       throw new errors.ApplicationError(
         `No puede editar el local.`
