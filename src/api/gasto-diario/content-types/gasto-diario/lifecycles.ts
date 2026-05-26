848bd0148c33befda77ed0cacaf12e21fff6e73b
jesustissera <jesustissera@gmail.com>
2026-05-26 10:13:02 -0300

    refactor: validar permisos antes de grabar

--- src/api/gasto-diario/content-types/gasto-diario/lifecycles.ts
+++ src/api/gasto-diario/content-types/gasto-diario/lifecycles.ts
@@ -1,15 +1,30 @@
 import { errors } from "@strapi/utils";
 const { ApplicationError } = errors;
 import { factories } from "@strapi/strapi";
+import { validateLocalPermissions } from "../../../../utils/validateLocalPermissions";
 
 export default {
   async beforeCreate(event) {
+    const { data } = event.params;
+    const ctx = strapi.requestContext.get();
+    const user = ctx.state.user;
+    if (data.local.connect && data.local.connect.length > 0) {
+      const localId = data.local.connect[0].id;
+      validateLocalPermissions(user, localId);
+    }
+
     (strapi as any).io.emit("refresh", "actualizado");
   },
-  async afterCreate(event) {
-    
-  },
+  async afterCreate(event) {},
   beforeUpdate(event) {
+    const { data } = event.params;
+    const ctx = strapi.requestContext.get();
+    const user = ctx.state.user;
+
+    if (data.local.connect && data.local.connect.length > 0) {
+      const localId = data.local.connect[0].id;
+      validateLocalPermissions(user, localId);
+    }
     (strapi as any).io.emit("refresh", "actualizado");
   },
 };
